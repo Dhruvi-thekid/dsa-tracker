40cf8c6e1b74dccd3b184cd333cf328d55c8be02
Dhruvi-thekid <sanganidhruvi2006@gmail.com>
2026-08-10 22:43:11 +0530

    feat: add file-based persistence

--- src/DSATracker.java
+++ src/DSATracker.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class DSATracker {
     private final ArrayList<Problem> problems = new ArrayList<>();
@@ -10,6 +14,7 @@ public class DSATracker {
             return;
         }
         problems.add(problem);
+        saveProblems();
         System.out.println("Problem added Successfully");
     }
 
@@ -68,8 +73,9 @@ public class DSATracker {
             System.out.println("Old Status: " + problem.getStatus());
 
             problem.setStatus(newStatus);
+            saveProblems();
 
-            System.out.println("New Status: " + problem.getStatus());
+            //System.out.println("New Status: " + problem.getStatus());
 
             System.out.println("Updated Successfully");
         }
@@ -85,6 +91,7 @@ public class DSATracker {
             return;
         }
         problems.remove(problem);
+        saveProblems();
 
         System.out.println("Deleted Successfully");
 
@@ -98,6 +105,69 @@ public class DSATracker {
             }
         }
         return null;
+    }
+
+    //to keep the data persistent
+    public void saveProblems(){
+
+        try{
+            FileWriter writer = new FileWriter("data.txt");
+
+            for(Problem problem : problems){
+                String line = 
+                problem.getId() +"|"+
+                problem.getTitle() +"|"+
+                problem.getPlatform() +"|"+
+                problem.getDifficulty() +"|"+
+                problem.getTopic()+"|"+
+                problem.getStatus();
+
+                writer.write(line);
+                writer.write('\n');
+            }
+            writer.close();
+        }
+        catch(IOException e){
+            System.out.println("Error, saving problem");
+        }
+    }
+
+    //to load the data from the file back to ArrayList to keep the data consistency
+    public void loadProblems(){
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
+
+            String line;
+            while((line = reader.readLine()) != null){
+                String data[] = line.split("\\|");
+
+                int id = Integer.parseInt(data[0]);
+                String title = data[1];
+                String platform = data[2];
+                Difficulty difficulty = Difficulty.valueOf(data[3].toUpperCase());
+                String topic = data[4];
+                Status status = Status.valueOf(data[5].trim().toUpperCase().replace(" ", "_"));
+
+                Problem problem = new Problem(
+                id,
+                title,
+                platform,
+                difficulty,
+                topic,
+                status);
+
+                problems.add(problem);
+
+            }
+                reader.close();
+        }
+        catch(IOException e){
+            System.out.println("Error loading data");
+        }
+    }
+
+    public DSATracker(){
+        loadProblems();
     }
 
     public void menu(){
